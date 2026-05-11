7c494763f560f0f1f69acc3d2e8e30da11cab051
Roman <romeo190107@gmail.com>
2026-05-11 18:19:51 +0500

    delete

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtCalendarApp_FILE /Users/parhom/Desktop/qt-calendar-app/build/QtCalendarApp.app/Contents/MacOS/QtCalendarApp)
-set(__QT_DEPLOY_TARGET_QtCalendarApp_TYPE EXECUTABLE)
--- build/CMakeFiles/QtCalendarApp_qmlimportscan.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt/qml_imports/QtCalendarApp_build.cmake"
-  "CMakeFiles/QtCalendarApp_qmlimportscan"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/QtCalendarApp_qmlimportscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
